a6cda00c6d0e2a7584d65bbbead2b8b1c5bbdddd
Gerard Duval <gduvalsc@gmail.com>
2016-12-21 18:02:14 +0100

    0,0,10

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ def testsuite():
 	test_suite = test_loader.discover('tests', pattern='test*.py')
 	return test_suite
 setup(name='daemonmgr',
-      version='0.0.9',
+      version='0.0.10',
       long_description=readme,
       description='Create daemons from commands',
       author='Gerard Duval',
       author_email='gerard.duval@gdsoftconsulting.com',
       url='https://github.com/gduvalsc/daemonmgr',
       scripts=['scripts/daemonmgr'],
-      license='BSD',
+      license='GPL',
       packages=['daemonmgr'],
-      classifiers=[ "Development Status :: 3 - Alpha", "Topic :: Utilities", "License :: OSI Approved :: BSD License"],
+      classifiers=[ "Development Status :: 3 - Alpha", "Topic :: Utilities", "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"],
       test_suite="setup.testsuite",
       zip_safe=False)
